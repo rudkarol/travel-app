c140954ff8548f30c8b20eb805cbd77a11cf7883
rudkarol <rudkowski.kr@proton.me>
2024-11-18 01:11:04 +0100

    get_current_user function and test email sending added to auth/router.py

--- auth/router.py
+++ auth/router.py
@@ -1,9 +1,14 @@
+from fastapi import Depends, HTTPException, status
 from datetime import datetime, timedelta, timezone
+from fastapi.security import HTTPBearer
+from typing import Annotated
 import jwt
+from jwt.exceptions import InvalidTokenError
 import string
 import secrets
 
 from config import get_settings
+from database import get_database
 
 
 settings = get_settings()
@@ -13,6 +18,10 @@ ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 VERIFICATION_CODE_LENGTH = 6
 VERIFICATION_CODE_EXPIRE_MINUTES = 15
+
+database = get_database()
+
+security = HTTPBearer()
 
 def generate_verification_code():
     """Generuje kod weryfikacyjny"""
@@ -29,3 +38,30 @@ def create_access_token(email: str):
     to_encode = {"sub": email, "exp": expire}
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
+
+
+async def get_current_user(token: Annotated[str, Depends(security)]):
+    """Weryfikuje JWT i zwraca użytkownika"""
+
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        return username
+    except InvalidTokenError:
+        raise credentials_exception
+
+
+async def send_verification_email(email: str, code: str):
+    """Wysyla email z kodem weryfikacyjnym"""
+
+    # TODO: obsluga email
+    print(f"Sending verification code {code} to {email}")
+
