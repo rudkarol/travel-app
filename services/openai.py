fcc985b1bed11dbd1b2157b4afdfbec5cb761f5b
rudkarol <rudkowski.kr@proton.me>
2024-12-13 20:09:29 +0100

    basic openai request

---
+++ services/openai.py
@@ -0,0 +1,31 @@
+from openai import OpenAI
+
+from dependencies import get_settings
+from schemas.locations import SearchResponse
+
+
+settings = get_settings
+
+
+def openai_request(places: SearchResponse):
+    # TODO: prompt, response model
+    prompt = ""
+
+    r = client.chat.completions.create (
+        model="gpt-4o-mini",
+        messages=[
+            {
+                "role": "system",
+                "content": "You are a helpful travel assistant."
+            },
+            {
+                "role": "user",
+                "content": prompt
+            }
+        ],
+        temperature=0.7,
+        max_tokens=254,
+        top_p=1
+    )
+    # TODO: convert response to pydantic model
+    return r.choices[0].message
