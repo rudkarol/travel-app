a32f951c4247a7166081f0c730e33bbec198e3e7
Karol Rudkowski <rudkowski.kr@proton.me>
2025-01-17 13:27:12 +0100

    xcode project - initial commit

---
+++ TravelApp/TravelApp/TravelAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelAppApp.swift
+//  TravelApp
+//
+//  Created by osx on 17/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct TravelAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
