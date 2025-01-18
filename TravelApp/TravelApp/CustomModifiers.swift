6390ae2338e5801c5d7af1a88244d73db15defa5
Karol Rudkowski <rudkowski.kr@proton.me>
2025-01-18 12:53:45 +0100

    custom bsmall button modifier

---
+++ TravelApp/TravelApp/CustomModifiers.swift
@@ -0,0 +1,17 @@
+//
+//  CustomModifiers.swift
+//  TravelApp
+//
+//  Created by osx on 18/01/2025.
+//
+
+import SwiftUI
+
+struct SmallButtonStyle: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .buttonStyle(.bordered)
+            .tint(.accentColor)
+            .controlSize(.small)
+    }
+}
