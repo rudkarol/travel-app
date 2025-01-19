59b2dab6934f5b7dc70f01af393320c9b005ea19
Karol Rudkowski <rudkowski.kr@proton.me>
2025-01-19 22:21:39 +0100

    basic location details page, favourites page api connection

---
+++ TravelApp/TravelApp/Views/Cells/InfoLinkCell.swift
@@ -0,0 +1,18 @@
+//
+//  InfoLinkCell.swift
+//  TravelApp
+//
+//  Created by osx on 19/01/2025.
+//
+
+import SwiftUI
+
+struct InfoLinkCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InfoLinkCell()
+}
