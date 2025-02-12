924809f7b424694a86f5214d092e8bd1f7673ea9
rudkarol <rudkowski.kr@proton.me>
2025-02-12 13:48:38 +0100

    deleting daily plan, empty daily plan text

--- TravelApp/TravelApp/Screens/PlansView/PlanPage.swift
+++ TravelApp/TravelApp/Screens/PlansView/PlanPage.swift
@@ -2,7 +2,7 @@
 //  PlanPage.swift
 //  TravelApp
 //
-//  Created by osx on 17/01/2025.
+//  Created by Karol Rudkowski on 17/01/2025.
 //
 
 import SwiftUI
@@ -12,6 +12,8 @@ struct PlanPage: View {
     @State var plan: Plan
     @State var sheetVisible: Bool = false
     @Binding var path: NavigationPath
+    @State private var dayToDelete: Int? = nil
+    @State private var isShowingDeleteAlert: Bool = false
     @State var alertData: AlertData?
     
     @Environment(PlansService.self) private var plansService
@@ -20,52 +22,72 @@ struct PlanPage: View {
     var body: some View {
         ZStack {
             List(plan.days.indices, id: \.self) { dayIndex in
-                Section("Day \(dayIndex + 1)") {
-                    ForEach((plan.days[dayIndex].places ?? []).indices, id: \.self) { placeIndex in
-                        let currentPlace = plan.days[dayIndex].places![placeIndex]
-                        
-                        NavigationLink(value: currentPlace) {
-                            PlaceListCell(location: currentPlace, showingAddToFavButton: false)
-                                .swipeActions(allowsFullSwipe: false) {
-                                    Button(role: .destructive) {
-                                        guard let planIndex = plansService.plans.firstIndex(where: { $0.id == plan.id }) else {
-                                            print("Plan index error")
-                                            return
-                                        }
-                                        
-                                        plan.days[dayIndex].places!.remove(at: placeIndex)
-                                        
-                                        var updatedPlan = plansService.plans[planIndex]
-                                        
-                                        if var places = updatedPlan.days[dayIndex].places {
-                                            places.remove(at: placeIndex)
-                                            updatedPlan.days[dayIndex].places = places
-                                            
-                                            plansService.plans[planIndex] = updatedPlan
-                                            
-                                            Task {
-                                                do {
-                                                    try await plansService.updatePlan(planId: plan.id)
-                                                } catch let error as AppError {
-                                                    alertData = error.alertData
-                                                } catch {
-                                                    alertData = AppError.genericError(error).alertData
+                        Section(
+                            header: HStack {
+                                Text("Day \(dayIndex + 1)")
+                                    .font(.headline)
+                                
+                                Spacer()
+                                
+                                Button {
+                                    dayToDelete = dayIndex
+                                    isShowingDeleteAlert = true
+                                } label: {
+                                    Image(systemName: "trash")
+                                        .foregroundColor(.secondary)
+                                }
+                                .buttonStyle(BorderlessButtonStyle())
+                            }
+                        ) {
+                            let places = plan.days[dayIndex].places ?? []
+                            if places.isEmpty {
+                                Text("The daily plan is empty")
+                                    .frame(maxWidth: .infinity, alignment: .center)
+                                    .foregroundColor(.secondary)
+                            } else {
+                                ForEach(places.indices, id: \.self) { placeIndex in
+                                    let currentPlace = places[placeIndex]
+                                    NavigationLink(value: currentPlace) {
+                                        PlaceListCell(location: currentPlace, showingAddToFavButton: false)
+                                            .swipeActions(allowsFullSwipe: false) {
+                                                Button(role: .destructive) {
+                                                    guard let planIndex = plansService.plans.firstIndex(where: { $0.id == plan.id }) else {
+                                                        print("Plan index error")
+                                                        return
+                                                    }
+                                                    
+                                                    plan.days[dayIndex].places?.remove(at: placeIndex)
+                                                    
+                                                    var updatedPlan = plansService.plans[planIndex]
+                                                    if var updatedPlaces = updatedPlan.days[dayIndex].places {
+                                                        updatedPlaces.remove(at: placeIndex)
+                                                        updatedPlan.days[dayIndex].places = updatedPlaces
+                                                        
+                                                        plansService.plans[planIndex] = updatedPlan
+                                                        
+                                                        Task {
+                                                            do {
+                                                                try await plansService.updatePlan(planId: plan.id)
+                                                            } catch let error as AppError {
+                                                                alertData = error.alertData
+                                                            } catch {
+                                                                alertData = AppError.genericError(error).alertData
+                                                            }
+                                                        }
+                                                    } else {
+                                                        print("Updating day data error")
+                                                    }
+                                                    
+                                                    print("Plan item deleted")
+                                                } label: {
+                                                    Label("Delete", systemImage: "trash")
                                                 }
                                             }
-                                        } else {
-                                            print("Błąd przy aktualizacji dnia lub miejsca")
-                                        }
-                                        
-                                        print("Plan item deleted")
-                                    } label: {
-                                        Label("Delete", systemImage: "trash")
                                     }
                                 }
-                            
+                            }
                         }
                     }
-                }
-            }
             .navigationDestination(for: Location.self) { location in
                 LocationDetailsView(location: location, path: $path)
             }
@@ -89,8 +111,35 @@ struct PlanPage: View {
             }
         }
         .sheet(isPresented: $sheetVisible) {
+            // TODO
             //            AddPlanForm(name: plan.name, description: plan.description ?? "", startDate: plan.startDate ?? Date(), toggleState: (plan.startDate != nil) ? true : false, toUpdate: true)
         }
+        .alert("Delete the day", isPresented: $isShowingDeleteAlert) {
+                    Button("Delete", role: .destructive) {
+                        if let dayIndex = dayToDelete,
+                           let planIndex = plansService.plans.firstIndex(where: { $0.id == plan.id }) {
+                            
+                            plan.days.remove(at: dayIndex)
+                            
+                            var updatedPlan = plansService.plans[planIndex]
+                            updatedPlan.days.remove(at: dayIndex)
+                            plansService.plans[planIndex] = updatedPlan
+                            
+                            Task {
+                                do {
+                                    try await plansService.updatePlan(planId: plan.id)
+                                } catch let error as AppError {
+                                    alertData = error.alertData
+                                } catch {
+                                    alertData = AppError.genericError(error).alertData
+                                }
+                            }
+                        }
+                    }
+                    Button("Cancel", role: .cancel) { }
+                } message: {
+                    Text("Are you sure you want to delete the whole day and all the places it contains?")
+                }
     }
 }
 
