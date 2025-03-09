d034399c58b4e9355b9a36da8d9e5bff82488fa4
rudkarol <rudkowski.kr@proton.me>
2025-03-09 12:58:28 +0100

    Logo view added, login page redesigned

---
+++ TravelApp/TravelApp/Views/Logo.swift
@@ -0,0 +1,18 @@
+//
+//  Logo.swift
+//  TravelApp
+//
+//  Created by Karol Rudkowski on 09/03/2025.
+//
+
+import SwiftUI
+
+struct Logo: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Logo()
+}
